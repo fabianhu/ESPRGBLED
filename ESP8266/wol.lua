f7dc44e58386e44ab2a35ca340eeb15611afa980
fabianhu <fabianwho@gmx.net>
2015-12-10 00:07:05 +0100

    beautified

--- ESP8266/wol.lua
+++ ESP8266/wol.lua
@@ -1,12 +1,16 @@
 -- send a WOL magic packet
+-- required: set up your wlan and adjust the MAC address and broadcast IP
 
-cu=net.createConnection(net.UDP)
--- cu:on("receive",function(cu,c) print(c) end)
-cu:connect(0,"192.168.1.255") -- to broadcast of this subnet
-arrhdr = "\255\255\255\255\255\255" -- WOL header
+broadcast_address = "192.168.1.255" -- of this subnet
+addr_MAC_str="00-11-32-31-08-ad" -- the address to be woken
 
-addr_hex_str="00-11-32-31-08-ad" -- the address to be woken
+arrhdr = "\255\255\255\255\255\255" -- magic packet header
 
+cu = net.createConnection(net.UDP) -- setup connection
+
+cu:connect(0,broadcast_address) -- connect to broadcast 
+
+-- helper stuff to dissect the MAC string
 local function split(str, sep)
    local result = {}
    local regex = ("([^%s]+)"):format(sep)
@@ -16,10 +20,10 @@ local function split(str, sep)
    return result
 end
 
-local lines = split(addr_hex_str, "-")
-
 arrmac="" -- the MAC in binary representation
-for r,line in ipairs(lines) do
+local single_bytes = split(addr_MAC_str, "-")
+-- create binary string of MAC address
+for r,line in ipairs(single_bytes) do
    local r = tonumber(line, 16)
    arrmac = arrmac..string.char(r)
 end
@@ -30,12 +34,12 @@ end
 --    print(string.byte(c))
 --end
 
-wolpacket = arrhdr -- build the magic packet header + 16x MAC
+wolpacket = arrhdr -- build the magic packet = header + 16x MAC
 for i=1,16 do
     wolpacket = wolpacket..arrmac
 end
 
-cu:send(wolpacket)
+cu:send(wolpacket) -- finally send the packet
 cu:close()
 
 
